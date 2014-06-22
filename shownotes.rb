5889f9b51ba6796ffcf46ed7df03ba95d51d83bf
Dominik Elberskirch <dominik.elberskirch@gmail.com>
2014-06-23 01:13:20 +0200

    - added pinboard_cmd to query different paths\n- added querying of all available notes

--- shownotes.rb
+++ shownotes.rb
@@ -1,6 +1,10 @@
 #!/usr/bin/env ruby
 require 'net/http'
 require 'uri'
+require 'json'
+
+API_ENDPOINT = "https://api.pinboard.in"
+API_VERSION = "v1"
 
 def load_api_token
    token_file = File.join(ENV["HOME"],".pinboard_token")
@@ -15,23 +19,31 @@ def load_api_token
    token
 end
 
-api_endpoint = "https://api.pinboard.in"
-api_version = "/v1/"
-api_token = load_api_token
-
-if api_token != nil 
-   notes = "notes/list/"
-   format = "format=json"
-   uri = URI.parse(api_endpoint)
+def pinboard_cmd(cmd, api_token)
+   params = { 'auth_token' => api_token, 'format' => 'json'}
+   puts cmd
+   path = [API_ENDPOINT, API_VERSION, cmd].join("/")
+   puts "path #{path}"
+   req_url = [path,URI.encode_www_form(params)].join("?") 
+   uri = URI.parse(req_url)
+   puts uri.inspect
    http = Net::HTTP.new(uri.host, uri.port)
    http.use_ssl = true
-
-   req_url = api_version + notes + "?auth_token=" + api_token + "&" + format
-   puts req_url
    req = Net::HTTP::Get.new(req_url)
    res = http.request(req)
+end
+##########################################
 
+api_token = load_api_token
+if api_token != nil 
+   res = pinboard_cmd("notes/list/", api_token)
    puts res.inspect
-   puts res.header.inspect
-   puts res.body.inspect
+   json = JSON.parse(res.body)
+   notes = json["notes"]
+   notes.each do |note|
+      id = note["id"]
+      res = pinboard_cmd("notes/#{id}",api_token)
+      puts res.body.inspect
+      sleep 1
+   end
 end
