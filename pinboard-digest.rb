be269f99a35f28f79e9f00c58ab7312170f3a46b
Dominik Elberskirch <dominik.elberskirch@gmail.com>
2014-09-18 23:59:29 +0200

    added recent_posts method\nchanged data handling to openstruct\nadded error handling for connection and commandline arguments

--- pinboard-digest.rb
+++ pinboard-digest.rb
@@ -32,17 +32,23 @@ module PinboardHelper
       if user_params != nil
          params = user_params.merge(params)
       end
-      puts cmd
+      #puts cmd
       path = [API_ENDPOINT, API_VERSION, cmd].join("/")
-      puts "path #{path}"
+      puts "Performing REST call: #{path}"
       req_url = [path,URI.encode_www_form(params)].join("?")
       uri = URI.parse(req_url)
-      puts uri.inspect
+      #puts uri.inspect
       http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = true
       req = Net::HTTP::Get.new(req_url)
-      res = http.request(req)
-      puts res.inspect
+      begin 
+         res = http.request(req)
+      rescue SocketError => se
+         puts "Caught a SocketError #{se}"
+         puts "Seems like you're not connected to a network"
+         exit 
+      end
+      #puts res.inspect
       res
    end
 
@@ -51,19 +57,12 @@ module PinboardHelper
    end
 end
 
-# get n last links, or last 7/14days, run it daily, if no links
-# linkstats + random link
-
 module PinboardDigest
    def self.html(data)
-      bookmarks = data["posts"]
-      user = data["user"]
-      date = data["date"]
-   
       template = haml_template
-      puts template
+      #puts template
       engine = Haml::Engine.new template
-      engine.render(Object.new, :bookmarks => bookmarks)
+      engine.render(Object.new, :bookmarks => data.bookmarks)
    end
 
    def self.haml_template
@@ -79,23 +78,19 @@ module PinboardDigest
    end
 
    def self.text(data)
-      bookmarks = data["posts"]
-      user = data["user"]
-      date = data["date"]
-
-      rows = bookmarks.map {|bm| 
+      rows = data.bookmarks.map {|bm| 
          [ PinboardHelper.truncate(bm["href"],40), 
          PinboardHelper.truncate(bm["description"],40), 
          bm["time"] ]
       }
-      title = sprintf("user %s\ndate %s", user, date)
+      title = sprintf("user %s\ndate %s", data.user, data.date)
       table = Terminal::Table.new :title => title, :rows => rows, :width => 25
    end
 
    def self.mailerconfig() 
       mailerconfig = JSON.parse(File.read("mailer.json"), :symbolize_names => true)
       @@mailer_options = mailerconfig[:smtp]
-      puts @@mailer_options.inspect
+      #puts @@mailer_options.inspect
    end
       
    def self.sendmail(to, subject, body)
@@ -109,50 +104,68 @@ module PinboardDigest
    end
 
    def self.parse(args)
-      puts args.inspect
+      #puts args.inspect
       options = OpenStruct.new
-      options.receiver = ""
-      options.max = 15
       options.html = false
 
       opt_parser = OptionParser.new do |opts|
+         opts.banner = "Usage: pinboard-digest.rb -x|--html --receiver EMAIL --max COUNT"
          opts.on("-r", "--receiver EMAIL", "EMAIL receiver of the PinboardDigest") do |r|
             options.receiver = r 
          end
 
-         opts.on("--max COUNT", Integer, "maximum COUNT of links per mail (1-100, default 15)") do |c|
+         opts.on("--max COUNT", Integer, "maximum COUNT of links per mail") do |c|
             if( c < 101 && c > 0 )
                options.max = c
             end
          end
 
          opts.on("-x", "--html", "use html format") do
-
             options.html = true
          end
       end
       opt_parser.parse!(args)
-      puts options.inspect
+      
+      if options.max == nil
+         puts opt_parser
+         exit
+      end
       options
+   end
+
+   def self.recent_posts(api_token, count)
+      posts = OpenStruct.new
+      res = PinboardHelper.request("posts/recent/",api_token, "count" => count)
+
+      #puts res.inspect
+      response_data = JSON.parse(res.body)
+      posts.user = response_data["user"]      
+      posts.bookmarks = response_data["posts"]   
+      posts.date = response_data["date"] 
+      posts 
+   end
+
+   def self.random_post()
    end
 
    def self.run(args)
       api_token = PinboardHelper.load_api_token
       options = parse(ARGV)
-
       if api_token != nil
-         res = PinboardHelper.request("posts/recent/",api_token, "count" => options.max)
-         response_data = JSON.parse(res.body)
+         posts = recent_posts(api_token, options.max)
          
          if options.html 
-            table = PinboardDigest.html(response_data)
+            table = PinboardDigest.html(posts)
          else 
-            table = PinboardDigest.text(response_data)
+            table = PinboardDigest.text(posts)
             table = table.to_s
          end
          puts table
-         mailerconfig # prepare mailer setup
-         sendmail(options.receiver, "PinboardDigest #{Time.now}",table)
+
+         if options.receiver != ""
+            mailerconfig # prepare mailer setup
+            sendmail(options.receiver, "PinboardDigest #{Time.now}",table)
+         end
       end
    end
 end
